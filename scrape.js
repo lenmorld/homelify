c19ab0e1afe14dc063ee868ece52aa31d9ed3dcc
lenmorld <lenmorld@gmail.com>
2019-09-08 14:17:52 -0400

    Scraping - update items if found, instead of duplicating

--- scrape.js
+++ scrape.js
@@ -3,11 +3,12 @@ const cheerio = require('cheerio');
 const db = require('diskdb');
 db.connect('./data', ['condos']);
 
-if (!db.condos.find().length) {
-	const condo = { id: "c1", features: "2 Bed 3 Bath", price: "$300000", thumbnail: "", images: [] };
-	db.condos.save(condo);
-}
-console.log(db.condos.find());
+// TEST CODE
+// if (!db.condos.find().length) {
+// 	const condo = { id: "c1", features: "2 Bed 3 Bath", price: "$300000", thumbnail: "", images: [] };
+// 	db.condos.save(condo);
+// }
+// console.log(db.condos.find());
 
 module.exports = function () {
 	axios.get('https://www.centris.ca/en/properties~for-sale~montreal-island?uc=1&view=Thumbnail').then(res => {
@@ -21,7 +22,7 @@ module.exports = function () {
 				// caregory: House | Condo
 				const real_estate = {
 					id: $(this).find('meta[itemprop="sku"]').attr('content'),
-					thumbnail: $(this).find('img[itemprop="image"]'),
+					thumbnail: $(this).find('img[itemprop="image"]').attr('src'),
 					category: $(this).find('.description .category div span').first().text().trim(),
 					address: $(this).find('.address').text().trim(),
 					features: $(this).find('.features').text().trim(),
@@ -31,8 +32,15 @@ module.exports = function () {
 
 				real_estates.push(real_estate);
 			})
-			// TODO: add to db
-			db.condos.save(real_estates);
+			// this currently duplicates all of them
+			// db.condos.save(real_estates);
+			// TODO loop and check one by one, or do an update instead
+			const update_options = {
+				multi: true, // update multiple
+				upsert: true // if object is not found, add it (update-insert)
+			}
+			var updated = db.articles.update({}, real_estates, update_options);
+			console.log(updated);
 		}
 	}, (err) => {
 		console.error(err);
