32b06ac9393c369e11dfa120f3f0b4aca2d2453a
lenmorld <lenmorld@gmail.com>
2019-09-08 19:03:00 -0400

    UI - initial view of /items

--- app/Homes.jsx
+++ app/Homes.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import axios from "axios";
 
 class Homes extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			homes: []
+		};
+	}
+
+	// TODO: use hooks here
+	componentDidMount() {
+		axios.get("/api/items").then(res => {
+			this.setState({
+				homes: res.data
+			});
+		});
+	}
+
 	render() {
-		return <h1>Find homes here!</h1>;
+		return (
+			<div>
+				<h1>Find homes here!</h1>
+				<hr />
+				{this.state.homes.map(home => (
+					<div key={home.id}>
+						<h3>
+							{home.address} : {home.price}
+						</h3>
+						<img loading="lazy" src={home.thumbnail} />
+					</div>
+				))}
+			</div>
+		);
 	}
 }
 
