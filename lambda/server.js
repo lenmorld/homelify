f9fdd7d3f97d41a8cfeafa17a6feac0f5f3d2f34
lenmorld <lenmorld@gmail.com>
2019-09-12 21:56:09 -0400

    separate server-local code with diskdb

--- lambda/server.js
+++ lambda/server.js
@@ -3,44 +3,24 @@ const express = require("express");
 const serverless = require('serverless-http');
 const server = express();
 const router = express.Router();
-const db = require('diskdb');
-db.connect('../data', ['condos', 'logs']);
 
-const scrape = require('../scrape');
+// const port = 4000;
 
-const port = 4000;
+// server.use(express.static("public"));
 
-server.use(express.static("public"));
-
-server.get("/", function (req, res) {
-	res.sendFile(__dirname + "/index.html");
-});
-
-server.get("/api/items", (req, res) => {
-	res.json(db.condos.find());
-})
+// server.get("/", function (req, res) {
+// 	res.sendFile(__dirname + "/index.html");
+// });
 
 // all backend routes go here under /api
-server.get("/api/json", function (req, res) {
-	res.json({ name: "Lenny" });
-});
+// server.get("/api/json", function (req, res) {
+// 	res.json({ name: "Lenny" });
+// });
 
 // server.listen(port, function () {
 // 	// Callback function
 // 	console.log("Running server at " + port);
 // });
-
-// RUN SCRAPER once a day
-// TODO: maybe put this in a config, .env file instead of db
-const timestamp = Date.now();
-const date_now = Math.floor(timestamp / (1000 * 60 * 60 * 24));
-
-const date_last_run = db.logs.find({
-	date_last_run: date_now
-});
-if (!date_last_run.length) {
-	scrape();
-}
 
 
 // Netlify Lambda
@@ -48,9 +28,10 @@ router.get("/", (req, res) => {
 	res.json("Hello Netlify Lambda");
 })
 
-router.get("/api/items", (req, res) => {
-	res.json(db.condos.find());
-})
+// TODO: GET FROM DB, since LAMBDA doesnt have disk access for diskdb
+// router.get("/api/items", (req, res) => {
+// 	res.json(db.condos.find());
+// })
 
 server.use('/.netlify/functions/server', router);
 
--- server-local.js
+++ server-local.js
@@ -1,6 +1,28 @@
+const db = require('diskdb');
+db.connect('./data', ['condos', 'logs']);
+
+const scrape = require('./scrape');
+
 const app = require('./lambda/server');
 
 const port = 4000;
+
+// RUN SCRAPER once a day
+// TODO: maybe put this in a config, .env file instead of db
+const timestamp = Date.now();
+const date_now = Math.floor(timestamp / (1000 * 60 * 60 * 24));
+
+const date_last_run = db.logs.find({
+	date_last_run: date_now
+});
+if (!date_last_run.length) {
+	scrape();
+}
+
+app.get("/api/items", (req, res) => {
+	res.json(db.condos.find());
+})
+
 
 app.listen(port, function () {
 	// Callback function
